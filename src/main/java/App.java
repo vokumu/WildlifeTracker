5da17352c8ab952dbe7b00bdf7c86a2cd41f65c2
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-20 04:41:17 +0300

    Added link to inex page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,12 @@
 public class App {
-}
+    public static void main(String[] args) {
+
+        // port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+    }
