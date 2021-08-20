f05e647f25b2bedf8e0b25e213f7cc32de9ae62d
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-20 04:46:46 +0300

    Added imports to app;

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,13 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
 
