705026ef31c1659cb467f43d99902ae466d4d29a
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-21 15:59:05 +0300

    Added files

---
+++ src/main/java/DB.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sql2o.Sql2o;
+import java.net.URI;
+import java.net.URISyntaxException;
+public class DB {
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-18-211-41-246.compute-1.amazonaws.com:5432/d6orupu6ca4o8m","jmibawcyjunppq","ec574f4c92cdb80d235426b083836cb6f3bd1b8e9eef80395c6aa4a8d4dbc07e");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-18-214-238-28.compute-1.amazonaws.com:5432/da7d70pm43spr7","buzonlhpartczi","b670e7f07b5b12334676089859c1e4a7af9bfe3b84e4011b101d04986cfcb12d");
+//    //    String connectionString = "jdbc:postgresql://@ec2-23-23-164-251.compute-1.amazonaws.com:5432/dfih1o8pai8045";
+////    Sql2o sql2o = new Sql2o(connectionString,"nhfwezodullugo","0aa74114bf808e5cc3ec83ce9d0a26c331d28b0270ca40c0d87c6284069e6eee");
+    }
+
---
+++ src/main/java/DatabaseProps.java
@@ -0,0 +1,5 @@
+public abstract class DatabaseProps {
+    public static String username = "denvar";
+
+    public static String password = "kenya@2020";
+}
